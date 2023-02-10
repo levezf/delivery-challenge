75578587f4fe4e6a47ca0b5fb063b13b754c4408
Felipe Levez <levezf@gmail.com>
2023-02-10 18:23:24 -0300

    Renomeando MainActivity para DeliveryChallengeActivity

--- app/src/main/java/br/com/levez/challenge/delivery/DeliveryChallengeActivity.kt
+++ app/src/main/java/br/com/levez/challenge/delivery/DeliveryChallengeActivity.kt
@@ -3,10 +3,10 @@ package br.com.levez.challenge.delivery
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class DeliveryChallengeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_delivery_challenge)
     }
 }
