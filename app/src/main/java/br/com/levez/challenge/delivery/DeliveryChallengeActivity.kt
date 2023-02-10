3e4b327698c6e5027718f46a1ff503a32456bf1d
Felipe Levez <levezf@gmail.com>
2023-02-10 18:32:02 -0300

    Adicionando Databinding a DeliveryChallengeActivity e navigation

--- app/src/main/java/br/com/levez/challenge/delivery/DeliveryChallengeActivity.kt
+++ app/src/main/java/br/com/levez/challenge/delivery/DeliveryChallengeActivity.kt
@@ -2,11 +2,18 @@ package br.com.levez.challenge.delivery
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import br.com.levez.challenge.delivery.databinding.ActivityDeliveryChallengeBinding
 
 class DeliveryChallengeActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityDeliveryChallengeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_delivery_challenge)
+        binding = DataBindingUtil.setContentView(
+            this@DeliveryChallengeActivity,
+            R.layout.activity_delivery_challenge
+        )
     }
 }
