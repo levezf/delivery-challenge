d8115226c0ea429886d2c407a91c708ef50b839c
Felipe Levez <levezf@gmail.com>
2023-02-12 19:05:22 -0300

    Criando um componente baseado no AutoCompleteTextView para manter o mesmo padráo do layout e tornar o campo funcional apenas por seleção

---
+++ app/src/main/java/br/com/levez/challenge/delivery/ui/common/customview/SelectableAutoCompleteTextView.kt
@@ -0,0 +1,34 @@
+package br.com.levez.challenge.delivery.ui.common.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.KeyEvent
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+
+
+class SelectableAutoCompleteTextView : MaterialAutoCompleteTextView {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
+
+    constructor(
+        context: Context,
+        attributeSet:
+        AttributeSet,
+        defStyleAttr: Int,
+    ) : super(context, attributeSet, defStyleAttr)
+
+    init {
+        threshold = 0
+        setOnClickListener {
+            if (adapter != null) {
+                performFiltering("", 0)
+                showDropDown()
+            }
+        }
+    }
+
+
+    override fun enoughToFilter(): Boolean = true
+}
