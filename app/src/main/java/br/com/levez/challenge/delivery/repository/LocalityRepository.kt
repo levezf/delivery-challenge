325ba350bf9d331b536c796fe76b56246c462c92
Felipe Levez <levezf@gmail.com>
2023-02-13 20:40:48 -0300

    Adicionando tratativa para exceções do Retrofit

--- app/src/main/java/br/com/levez/challenge/delivery/repository/LocalityRepository.kt
+++ app/src/main/java/br/com/levez/challenge/delivery/repository/LocalityRepository.kt
@@ -1,10 +1,12 @@
 package br.com.levez.challenge.delivery.repository
 
 import br.com.levez.challenge.delivery.model.City
-import br.com.levez.challenge.delivery.network.LocalityApi
+import br.com.levez.challenge.delivery.network.api.LocalityApi
 
 class LocalityRepository(
     private val localityApi: LocalityApi,
 ) {
-    suspend fun getCitiesByState(state: String): List<City> = localityApi.getCityByState(state)
+    suspend fun getCitiesByState(state: String): List<City> = runCatching {
+        localityApi.getCityByState(state)
+    }.getOrDefault(emptyList())
 }
